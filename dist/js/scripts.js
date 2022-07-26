2c4ae4a9244f98b2c4536c760a98114b1a92b660
Izvio Prijaya Mumin <prijayaizvio@gmail.com>
2022-07-26 13:39:23 +0700

    Add Feature Dark Mode

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -31,3 +31,28 @@ window.addEventListener('click', function (e) {
     navMenu.classList.add('hidden');
   }
 });
+
+// Dark Mode Toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function () {
+  if (darkToggle.checked) {
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
+  } else {
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+});
+
+// Pindahkan posisi toggle sesuai mode
+if (
+  localStorage.theme === 'dark' ||
+  (!('theme' in localStorage) &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
